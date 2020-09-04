e0676f907db510e4369753dfeed0b9213e47ba8f
Jason Baumohl <jkbaumohl@lbl.gov>
2020-09-04 02:39:05 +0000

    trying from Genome

--- lib/ProkkaAnnotation/Util/ProkkaUtils.py
+++ lib/ProkkaAnnotation/Util/ProkkaUtils.py
@@ -432,14 +432,14 @@ class ProkkaUtils:
                         ec_numbers = qualifiers["eC_number"]
                         sso_terms = dict()
                         for ec in ec_numbers:
-#                            if ec and ec in self.ec_lookup_dictionary:
-#                                ec_terms = {}
-#                                ec_terms[ec] = {"id": ec,
-#                                                "evidence": [evidence],
-#                                                "term_name": self.ec_lookup_dictionary[ec],
-#                                                "term_lineage": []}
-#                                feature["ontology_terms"] = {"EC": ec_terms}
-#                                genes_with_ec += 1
+                            if ec and ec in self.ec_lookup_dictionary:
+                                ec_terms = {}
+                                ec_terms["EC:" + ec] = {"id": "EC:" + ec,
+                                                        "evidence": [evidence],
+                                                        "term_name": self.ec_lookup_dictionary[ec],
+                                                        "term_lineage": []}
+                                feature["ontology_terms"] = {"EC": ec_terms}
+                                genes_with_ec += 1
                             sso_list = self.ec_to_sso.get(ec, [])
                             for sso_item in sso_list:
                                 sso_terms[sso_item["id"]] = {"id": sso_item["id"],
