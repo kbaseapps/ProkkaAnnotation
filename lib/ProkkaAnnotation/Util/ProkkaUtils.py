5a27aee7091fa8bc44849e878e1583d120bf946b
Jason Baumohl <jkbaumohl@lbl.gov>
2020-09-04 07:05:32 +0000

    trying from Genome

--- lib/ProkkaAnnotation/Util/ProkkaUtils.py
+++ lib/ProkkaAnnotation/Util/ProkkaUtils.py
@@ -550,7 +550,7 @@ class ProkkaUtils:
             genome_data['data']['ontology_events'] = [ec_event]
 
         genome_obj_modified = namedtuple('genome_obj_modified', 'genome_data ec_ontology_event_index')
-        return (genome_obj_modified(genome_data, ec_ontology_event_index),ec_ontology_event_index)
+        return genome_obj_modified(genome_data, ec_ontology_event_index)
     
     def create_genome_ontology_fields(self, genome_data):
         """
